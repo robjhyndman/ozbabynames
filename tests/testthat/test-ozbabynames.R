2db17ead8e80ba0caa4fb1ac65a446e56ed3e81e
Rob Hyndman <robjhyndman@gmail.com>
2024-10-23 09:04:46 +1100

    Added unit tests

---
+++ tests/testthat/test-ozbabynames.R
@@ -0,0 +1,8 @@
+test_that("ozbabynames", {
+  expect_equal(length(unique(ozbabynames$state)), 7L)
+  expect_equal(min(ozbabynames$year), 1930)
+  expect_true(max(ozbabynames$year) < 2024)
+  expect_equal(class(ozbabynames$year), "integer")
+  expect_equal(class(ozbabynames$count), "integer")
+  expect_equal(colnames(ozbabynames), c("year", "state", "sex", "name", "count"))
+})
