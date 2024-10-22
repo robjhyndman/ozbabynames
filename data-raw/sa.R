55bde440f484399dce093e45f6c1c98287333fd0
Rob Hyndman <robjhyndman@gmail.com>
2024-10-23 09:05:57 +1100

    Filter out totals

--- data-raw/sa.R
+++ data-raw/sa.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
-sa <- read_csv("data-raw/sa/merged/sa_babynames.csv") %>%
-  select(name, sex, year, number) %>%
-  rename(count = number) %>%
-  mutate(year = as.integer(year)) %>%
+sa <- read_csv("data-raw/sa/merged/sa_babynames.csv") |>
+  select(name, sex, year, number) |>
+  rename(count = number) |>
+  mutate(year = as.integer(year)) |>
   filter(!is.na(name))
