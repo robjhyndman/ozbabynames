7f97911102c9e292010b02a5894276f83fbd87ee
Rob J Hyndman <robjhyndman@gmail.com>
2025-03-02 14:14:33 +1100

    Updated NSW to 2024

--- data-raw/download_nsw.R
+++ data-raw/download_nsw.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 # https://data.nsw.gov.au/data/dataset?q=baby%20names
 
-csv_url <- "https://data.nsw.gov.au/data/dataset/a677cbe2-91e1-4e45-b771-08830d3d9e41/resource/2adcb228-9101-4c95-a786-b3216539b4a2/download/popular_baby_names_1952_to_2023.csv"
+csv_url <- "https://data.nsw.gov.au/data/dataset/a677cbe2-91e1-4e45-b771-08830d3d9e41/resource/2adcb228-9101-4c95-a786-b3216539b4a2/download/popular_baby_names_1952_to_2024.csv"
 
 bn_data <- read_csv(csv_url) |>
   select(-Rank) |>
